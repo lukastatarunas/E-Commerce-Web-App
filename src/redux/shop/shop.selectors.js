2ccbb5eaf1f740ced452ca3b5934e688dceafa22
lukas.tatarunas@shawk.eu <lukas.tatarunas@shawk.eu>
2021-04-08 18:59:23 +0300

    add shop data to the firestore and synchronize it with the app and and spinner HOC

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -5,8 +5,8 @@ const selectShop = (state) => state.shop;
 export const selectShopCollections = createSelector([selectShop], (shop) => shop.collections);
 
 export const selectShopCollectionsForPreview = createSelector([selectShopCollections], (collections) =>
-    Object.keys(collections).map((key) => collections[key]),
+    collections ? Object.keys(collections).map((key) => collections[key]) : [],
 );
 
 export const selectShopCollection = (collectionUrlParam) =>
-    createSelector([selectShopCollections], (collections) => collections[collectionUrlParam]);
+    createSelector([selectShopCollections], (collections) => (collections ? collections[collectionUrlParam] : null));
